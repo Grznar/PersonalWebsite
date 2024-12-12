944c702aba83bf78285ecf8369e8b03831c805ae
Jakub Černý <AsKronak@seznam.cz>
2024-12-12 19:28:46 +0100

    Update

--- priklady/priklad1/script.js
+++
@@ -1,6 +0,0 @@
-var tlacitko= document.querySelector(".tlacitko button");
-var menu = document.querySelector(".menu");
-tlacitko.addEventListener("click", function()
-{
-menu.classList.toggle("menu--open");
-});
